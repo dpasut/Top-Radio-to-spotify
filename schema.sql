0fc2c67b62f872e1acb80ef661314aecadae8e74
Wesley Taylor <jghjgfgh@gmail.com>
2017-03-23 06:23:16 -0400

    New `last_week_songs` view.

--- schema.sql
+++ schema.sql
@@ -15,3 +15,11 @@ SELECT json_extract(song, '$.artist_name') AS artist,
        json_extract(song, '$.song_name') AS song,
        json_extract(song, '$.last_played') AS play_time
 FROM base;
+
+CREATE VIEW IF NOT EXISTS last_week_songs
+AS
+SELECT artist, song, count(*) AS play_count
+FROM songs
+WHERE play_time >= CAST(strftime('%s', datetime('now', '-7 days')) AS INTEGER)
+GROUP BY artist, song
+ORDER BY count(*) DESC;

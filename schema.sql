3db19aa27c18bd4a35c7f1880e19ed82c1d0b0e7
Wesley Taylor <jghjgfgh@gmail.com>
2017-03-23 03:53:27 -0400

    New schema for SQLite3.

--- schema.sql
+++ schema.sql
@@ -1,17 +1,17 @@
-CREATE TABLE raw_data
+CREATE TABLE IF NOT EXISTS raw_data
 (
   date TIMESTAMPTZ NOT NULL PRIMARy KEY,
   data JSONB
 );
 
-CREATE VIEW songs
-AS
-with base as
-(
-  select jsonb_array_elements(data->'data'->'songs') as song
-  from raw_data
-)
-select song->>'artist_name' as artist,
-       song->>'song_name' as song,
-       to_timestamp((song->>'last_played')::bigint) as play_time
-from base;
+-- CREATE VIEW songs
+-- AS
+-- with base as
+-- (
+--   select jsonb_array_elements(data->'data'->'songs') as song
+--   from raw_data
+-- )
+-- select song->>'artist_name' as artist,
+--        song->>'song_name' as song,
+--        to_timestamp((song->>'last_played')::bigint) as play_time
+-- from base;

7fe31572b32e1c86e215e624ba39380ba62c5f81
Wesley Taylor <jghjgfgh@gmail.com>
2017-03-23 08:55:06 -0400

    Fix token get script to *not* open a browser.

--- get-spotify-token.py
+++ get-spotify-token.py
@@ -2,7 +2,7 @@
 
 import sys
 import json
-import spotipy.util as util
+import spotipy.oauth2 as oauth2
 
 scopes = ['playlist-modify-public', 'playlist-modify-private',
           'playlist-read-private', 'playlist-read-collaborative']
@@ -14,12 +14,23 @@ else:
     sys.exit()
 
 data = json.load(open('spotify-creds.json'))
-token = util.prompt_for_user_token(username, ' '.join(scopes),
-                                   data['id'],
-                                   data['secret'],
-                                   "http://localhost/")
+sp_oauth = oauth2.SpotifyOAuth(data['id'], data['secret'],
+                               "http://localhost/",
+                               scope=' '.join(scopes),
+                               cache_path=".cache-" + username)
+token_info = sp_oauth.get_cached_token()
 
-if token:
-    print(token)
+if not token_info:
+    auth_url = sp_oauth.get_authorize_url()
+    print("Please navigate here: {}".format(auth_url))
+    print()
+    response = raw_input("Enter the URL you were redirected to: ")
+    print()
+    code = sp_oauth.parse_response_code(response)
+    token_info = sp_oauth.get_access_token(code)
+
+if token_info:
+    print('Your token is:')
+    print(token_info['access_token'])
 else:
     print("Can't get token for ", username)

18dbb3fa6667a147eafd06be3e5a11d0a2aa8fde
Wesley Taylor <jghjgfgh@gmail.com>
2017-03-23 08:59:42 -0400

    Better arg. parsing for token script.

--- get-spotify-token.py
+++ get-spotify-token.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python2
 
-import sys
 import json
 import spotipy.oauth2 as oauth2
+import argparse
 
 scopes = ['playlist-modify-public', 'playlist-modify-private',
           'playlist-read-private', 'playlist-read-collaborative']
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        username = sys.argv[1]
-    else:
-        print("Usage: {} username".format(sys.argv[0]))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('username')
+    parser.add_argument('--force', action='store_true')
+
+    args = parser.parse_args()
 
     data = json.load(open('spotify-creds.json'))
     sp_oauth = oauth2.SpotifyOAuth(data['id'], data['secret'],
                                    "http://localhost/",
                                    scope=' '.join(scopes),
-                                   cache_path=".cache-" + username)
+                                   cache_path=".cache-" + args.username)
     token_info = sp_oauth.get_cached_token()
 
     if not token_info:
@@ -33,4 +34,4 @@ if __name__ == '__main__':
         print('Your token is:')
         print(token_info['access_token'])
     else:
-        print("Can't get token for ", username)
+        print("Can't get token for ", args.username)

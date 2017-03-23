9184f7beeb6626c969bbd1f1e5b8f43e0056f1ba
Wesley Taylor <jghjgfgh@gmail.com>
2017-03-23 09:21:24 -0400

    Stub file for all the playlist stuff.

---
+++ playlist.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+
+DATA = [("Milky Chance", "Cocoon"),
+        ("Rag'n'bone Man", "Human"),
+        ("Twenty One Pilots", "Heavydirtysoul"),
+        ("Japandroids", "Near To The Wild Heart Of Life"),
+        ("Mother Mother", "The Drugs"),
+        ("Green Day", "Still Breathing"),
+        ("Cage The Elephant", "Cold Cold Cold"),
+        ("Imagine Dragons", "Believer"),
+        ("Bastille", "Blame"),
+        ("Kings Of Leon", "Reverend")]
+
+if __name__ == '__main__':
+    # list playlists
+    # create if not exist
+    # get contents
+    # compare to DATA
+    # if different, change to match

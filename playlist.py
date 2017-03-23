60af4eb2c33ca20a8e9fb6331f0d01d0b9fdb311
Daniel Pasut <daniel.j.pasut@gmail.com>
2017-03-23 19:23:39 -0400

    Added check for playlist name/create new playlist

--- playlist.py
+++ playlist.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python2
+import sys
+from pprint import pprint
+
+import spotipy
+import spotipy.util as util
+from credentials import Credentials
+import json
+
 
 DATA = [("Milky Chance", "Cocoon"),
         ("Rag'n'bone Man", "Human"),
@@ -12,9 +20,36 @@ DATA = [("Milky Chance", "Cocoon"),
         ("Kings Of Leon", "Reverend")]
 
 if __name__ == '__main__':
+    cred = Credentials()
+
     # list playlists
     # create if not exist
     # get contents
     # compare to DATA
     # if different, change to match
     # https://developer.spotify.com/web-api/replace-playlists-tracks/
+
+    token = cred.spotify_user_access_token
+
+
+    sp = spotipy.Spotify(auth=token)
+    sp.trace = False
+    user = sp.current_user()
+    username = user['id']
+
+    results = sp.current_user_playlists(limit=50)
+
+    names = [a['name'] for a in results['items']]
+
+    need_new = True
+
+
+    for name in names:
+        if name == "Top 100 on The Edge":
+            #update playlist with new top 100
+            print("Playlist already exists, update instead")
+            need_new = False
+
+
+    if need_new == True:
+        playlists = sp.user_playlist_create(username,"Top 100 on The Edge")

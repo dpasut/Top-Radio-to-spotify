5acbf802a90701ae49ad3d2a7d48515a142e9846
Wesley Taylor <jghjgfgh@gmail.com>
2017-03-24 01:10:10 -0400

    Get rid of hardcoded 83; proper datetime subtract.

--- load.py
+++ load.py
@@ -4,14 +4,16 @@ import json
 import requests
 import sqlite3
 from tqdm import tqdm
+from datetime import datetime
 
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
 BASE_LINK = 'http://www.edge.ca/api/v1/music/broadcastHistory?accountID=36&day=-{}'
 
 if __name__ == '__main__':
+    day_range = (datetime.today() - datetime(2016, 12, 29)).days
     with sqlite3.connect('data.db') as conn:
         conn.executescript(open('schema.sql').read())
-        for i in tqdm(range(1, 83)):
+        for i in tqdm(range(day_range + 1)):
             data = requests.get(BASE_LINK.format(i),
                                 headers={'User-Agent': USER_AGENT}).json()
             date = data['data']['startDate']

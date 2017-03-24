2b672737038c3455ce0621dd73067237e6c9db52
Wesley Taylor <jghjgfgh@gmail.com>
2017-03-24 16:16:36 -0400

    Support refreshing access tokens when there's a cache.

--- credentials.py
+++ credentials.py
@@ -1,5 +1,8 @@
 import json
+import spotipy.oauth2 as oauth2
 
+scopes = ['playlist-modify-public', 'playlist-modify-private',
+          'playlist-read-private', 'playlist-read-collaborative']
 
 class Credentials:
     def __init__(self, fn='creds.json'):
@@ -12,3 +15,12 @@ class Credentials:
         self.spotify_secret = data['spotify_secret']
         self.spotify_user_access_token = data['spotify_user_access_token']
         self.spotify_username = data['spotify_username']
+        if self.spotify_username != '':
+            sp_oauth = oauth2.SpotifyOAuth(self.spotify_id,
+                                           self.spotify_secret,
+                                           "http://localhost/",
+                                           scope=' '.join(scopes),
+                                           cache_path=".cache-" + self.spotify_username)
+            token_info = sp_oauth.get_cached_token()
+            if token_info:
+                self.spotify_user_access_token = token_info['access_token']

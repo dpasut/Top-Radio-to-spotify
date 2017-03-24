0e582a30d05e9dade6ede10b8de49daf34e881f3
Wesley Taylor <jghjgfgh@gmail.com>
2017-03-24 02:09:42 -0400

    Get live update stuff working. Still issue with timezone.

--- load.py
+++ load.py
@@ -10,9 +10,16 @@ USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Ge
 BASE_LINK = 'http://www.edge.ca/api/v1/music/broadcastHistory?accountID=36&day=-{}'
 
 if __name__ == '__main__':
-    day_range = (datetime.today() - datetime(2016, 12, 29)).days
     with sqlite3.connect('data.db') as conn:
         conn.executescript(open('schema.sql').read())
+        cur = conn.cursor()
+        # TODO: Fix timezone crap
+        cur.execute("select coalesce(max(date(date)), '2016-12-29') from raw_data")
+        min_date = cur.fetchone()[0]
+        min_date = datetime(int(min_date[0:4]),
+                            int(min_date[5:7]),
+                            int(min_date[8:10]))
+        day_range = (datetime.today() - min_date).days
         for i in tqdm(range(day_range + 1)):
             data = requests.get(BASE_LINK.format(i),
                                 headers={'User-Agent': USER_AGENT}).json()

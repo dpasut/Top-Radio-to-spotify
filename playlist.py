0165f51c006772c703ae1d88712bae2106050824
Daniel Pasut <daniel.j.pasut@gmail.com>
2017-03-29 19:55:30 -0400

    Combine load.py and playlist.py

--- playlist.py
+++ playlist.py
@@ -10,6 +10,18 @@ import json
 import sqlite3
 import re
 
+import requests
+import sqlite3
+from datetime import datetime
+
+    # list playlists
+    # create if not exist
+    # update playlist(s)
+    # https://developer.spotify.com/web-api/replace-playlists-tracks/
+
+
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
+BASE_LINK = 'http://www.edge.ca/api/v1/music/broadcastHistory?accountID=36&day=-{}'
 
 
 def find_track_id(artist_name,track_name,track_ids):
@@ -22,18 +34,34 @@ def find_track_id(artist_name,track_name,track_ids):
         track_ids.append(track_id)
         break
 
+
 if __name__ == '__main__':
+
+    with sqlite3.connect('data.db') as conn:
+        conn.executescript(open('schema.sql').read())
+        cur = conn.cursor()
+        # TODO: Fix timezone crap
+        cur.execute("select coalesce(max(date(date)), '2016-12-29') from raw_data")
+        min_date = cur.fetchone()[0]
+        min_date = datetime(int(min_date[0:4]),
+                            int(min_date[5:7]),
+                            int(min_date[8:10]))
+        day_range = (datetime.today() - min_date).days
+        for i in tqdm(range(day_range + 1)):
+            data = requests.get(BASE_LINK.format(i),
+                                headers={'User-Agent': USER_AGENT}).json()
+            date = data['data']['startDate']
+            conn.execute("""
+                         INSERT OR REPLACE INTO raw_data (date, data)
+                         SELECT datetime(?, 'start of day'), ?
+                         """,
+                         (date, json.dumps(data)))
+            conn.commit()
+
+        cur.execute('select * from last_week_songs order by play_count desc, random()')
+        song_data = cur.fetchall()
+
     cred = Credentials()
-    conn = sqlite3.connect("data.db")
-    c = conn.cursor()
-    c.execute('select * from last_week_songs order by play_count desc, random() limit 100')
-    data = c.fetchall()
-
-    # list playlists
-    # create if not exist
-    # update playlist(s)
-    # https://developer.spotify.com/web-api/replace-playlists-tracks/
-
     token = cred.spotify_user_access_token
 
     sp = spotipy.Spotify(auth=token)
@@ -61,8 +89,8 @@ if __name__ == '__main__':
         playlist_id = playlists['id']
 
 
-    for i in tqdm(range(min(len(data),100))):
-        find_track_id(str(data[i][0]),str(data[i][1]),track_ids)
+    for i in tqdm(range(min(len(song_data),100))):
+        find_track_id(str(song_data[i][0]),str(song_data[i][1]),track_ids)
 
     print(len(track_ids), 'songs uploaded to spotify! Enjoy!')
     tracks = sp.user_playlist_replace_tracks(username, playlist_id, track_ids)
